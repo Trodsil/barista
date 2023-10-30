204df0283fd20aa11fc73dc075e0116a30202827
trodsil <tiagoalfonsorodriguezsilva@gmail.com>
2023-10-30 20:27:55 +0100

    commit # 3

--- src/classes/Barista.java
+++ src/classes/Barista.java
@@ -5,16 +5,16 @@ public class Barista extends Thread{
 
     private Cliente cliente;
 
-    private boolean ocupada = false;
+    private volatile boolean ocupada = false;
 
     @Override
     public void run() {
-
+        String nombre = cliente.getNombre();
         try {
             Thread.sleep(5000L);
-            System.out.println("Se esta preparando el café de "+cliente.getNombre());
+            System.out.println("Se esta preparando el café de "+nombre);
             Thread.sleep(tiempopreparacion*1000L);
-            System.out.println(cliente.getNombre()+" ha conseguido el café");
+            System.out.println(nombre+" ha conseguido el café");
             this.ocupada = false;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
